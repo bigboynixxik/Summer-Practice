3dd8e16d8ad41802f9f01b8ddd586f1b9b794ad6
Nixxik <v.gorbachev05@yandex.ru>
2024-06-07 13:37:48 +0700

    First design

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from PyQt5 import QtWidgets
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from mydesign import UiMainWindow
 
+if __name__ == "__main__":
+    import sys
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = UiMainWindow(MainWindow)
+    ui.setup_ui()
+    MainWindow.show()
+    sys.exit(app.exec_())
